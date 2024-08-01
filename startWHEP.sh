fcf72d36dbe4c9b017018eb7cbd6a9b1af11e6c5
T H Panton <thp@westhawk.co.uk>
2024-08-01 19:07:30 +0100

    Changed path for opus codec

--- startWHEP.sh
+++ startWHEP.sh
@@ -1,2 +1,2 @@
 #!/bin/sh 
-LD_LIBRARY_PATH=`pwd`/phono_opus java -cp target/whipi-1.2.jar pe.pi.whipi.WhepCommandLine $1 $2
+LD_LIBRARY_PATH=`pwd`/src/main/resources/phono_opus java -cp target/whipi-1.2.jar pe.pi.whipi.WhepCommandLine $1 $2
